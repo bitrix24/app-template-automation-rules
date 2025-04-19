d87859b5151ccc91c45beeba98b7913865b33076
IgorShevchik <s.hevchik.mail@gmail.com>
2025-04-19 09:21:40 +0300

    chore(component/order): get deal / products and show

--- frontend/app/components/render/Order.vue
+++ frontend/app/components/render/Order.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import { inject, ref } from 'vue'
+import { EnumCrmEntityTypeId } from '@bitrix24/b24jssdk'
+import type { B24Hook, LoggerBrowser } from '@bitrix24/b24jssdk'
 import SettingsIcon from '@bitrix24/b24icons-vue/common-service/SettingsIcon'
 
-defineProps({
+const props = defineProps({
   taskId: String,
   status: {
     type: String,
     default: 'processing'
   }
 })
+
+const $b24 = inject('$b24') as B24Hook
+const $logger = inject('$logger') as LoggerBrowser
 
 const currentDateTime = ref(new Date().toLocaleString(
   'en-US',
@@ -20,7 +26,62 @@ const currentDateTime = ref(new Date().toLocaleString(
     hour: '2-digit',
     minute: '2-digit',
     second: '2-digit'
-  }))
+  }
+))
+
+const deal = ref<{
+  id: number
+  title: string
+  products: {
+    id: number
+    xmlId: string
+    productId: number
+    productName: string
+    price: number
+    quantity: number
+    measureName: string
+    type: number
+    storeId: number
+  }[]
+}>({
+  id: 0,
+  title: '',
+  products: []
+})
+
+try {
+  const response = await $b24.callMethod(
+    'crm.item.get',
+    {
+      entityTypeId: EnumCrmEntityTypeId.deal,
+      id: props.taskId,
+      select: [
+        'id',
+        'title'
+      ],
+      order: {
+        id: 'desc'
+      }
+    }
+  )
+  deal.value = response.getData().result?.item
+
+  /**
+   * @todo get more > 50
+   */
+  const responseP = await $b24.callMethod(
+    'crm.item.productrow.list',
+    {
+      filter: {
+        '=ownerType': 'D',
+        '=ownerId': props.taskId
+      }
+    }
+  )
+  deal.value.products = responseP.getData().result?.productRows || []
+} catch (error) {
+  $logger.error(error)
+}
 </script>
 
 <template>
@@ -28,7 +89,7 @@ const currentDateTime = ref(new Date().toLocaleString(
     <h1 class="text-2xl font-bold mb-4">
       Task #{{ taskId }} <SettingsIcon class="size-5" />
     </h1>
-    <div class="space-y-2">
+    <div class="space-y-2 mb-4">
       <p class="text-base-600">
         Status: <span class="font-medium">{{ status }}</span>
       </p>
@@ -36,5 +97,16 @@ const currentDateTime = ref(new Date().toLocaleString(
         Created: {{ currentDateTime }}
       </p>
     </div>
+    <ProseH3>{{ deal.title }}</ProseH3>
+    <small>id: {{ deal.id }}</small>
+    <ul>
+      <li
+        v-for="(product, key) in deal.products"
+        :key="key"
+      >
+        <ProseH3>{{ product.productName }} <small>id: {{ product.productId }}</small></ProseH3>
+        <ProseP>{{ product.price }} x {{ product.quantity }} {{ product.measureName }}</ProseP>
+      </li>
+    </ul>
   </div>
 </template>
