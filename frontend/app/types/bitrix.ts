660e275775359c830c530ba14cc51e9a2f9bd7b7
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-04-25 17:15:22 +0300

    chore(type): add ProductImage, ProductInfo

--- frontend/app/types/bitrix.ts
+++ frontend/app/types/bitrix.ts
@@ -1,3 +1,5 @@
+import type { BoolString } from '@bitrix24/b24jssdk'
+
 export type BatchCommands = {
   [key: string]: string | {
     method: string
@@ -15,6 +17,30 @@ export type BitrixBatchResponse<T extends Record<string, any>> = {
   }
 }
 
+export type ProductImage = {
+  id: number
+  name: string
+  productId: number
+  type: 'DETAIL_PICTURE' | 'PREVIEW_PICTURE' | 'MORE_PHOTO'
+  downloadUrl?: string
+  detailUrl?: string
+}
+
+export type ProductInfo = {
+  id: number
+  active: BoolString
+  iblockId: number
+  name: string
+  previewText?: string
+  previewTextType?: 'text' | 'html'
+  detailText?: string
+  detailTextType?: 'text' | 'html'
+  weight?: number
+  height?: number
+  length?: number
+  width?: number
+}
+
 export type ProductRow = {
   id: number
   xmlId: string
@@ -25,6 +51,8 @@ export type ProductRow = {
   quantity: number
   measureName: string
   storeId: number
+  productInfo?: ProductInfo
+  productImage?: ProductImage[]
 }
 
 export type Entity = {
