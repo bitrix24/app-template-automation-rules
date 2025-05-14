30dd59a60174a53dcca30ff472021acf9cd32cf9
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-05-14 10:20:43 +0300

    app: improve

--- frontend/server/rabbitmq.config.ts
+++ frontend/server/rabbitmq.config.ts
@@ -1,20 +1,10 @@
-import { config } from 'dotenv'
 import type { RabbitMQConfig } from '@bitrix24/b24rabbitmq'
 
-config({ path: '.env' })
-/**
- * @todo fix this - test for Docker
- * @todo return to appConfig
- */
-// console.warn('Config env', {
-//   NUXT_RABBITMQ_URL: process.env?.NUXT_RABBITMQ_URL || '?'
-// })
-
-const rabbitmqUrl = process.env?.NUXT_RABBITMQ_URL || '?'
+const config = useRuntimeConfig()
 
 export const rabbitMQConfig: RabbitMQConfig = {
   connection: {
-    url: rabbitmqUrl,
+    url: config.rabbitmqUrl,
     reconnectInterval: 5000,
     maxRetries: 5
   },
