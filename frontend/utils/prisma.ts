0d01d2630985c33b01003214a46d0539d30da897
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-05-16 08:00:39 +0300

    chore(prisma): success config

--- frontend/utils/prisma.ts
+++ frontend/utils/prisma.ts
@@ -1,5 +1,3 @@
-import { PrismaClient } from '@prisma/client'
-// @memo wait schema.prisma
-// import { PrismaClient } from './../prisma/generated/prisma/client'
+import { PrismaClient } from './../generated/prisma/client'
 
 export const prisma = new PrismaClient()
