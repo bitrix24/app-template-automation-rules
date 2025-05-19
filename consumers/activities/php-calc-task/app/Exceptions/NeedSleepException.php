eab15f71f2f51ffd987f9703da9718a76118b383
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-05-19 09:45:42 +0300

    chore: improve

--- consumers/activities/php-calc-task/app/Exceptions/NeedSleepException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace AppCrmEntityTaskCalc\Exceptions;
-
-class NeedSleepException extends \Exception {}
--- consumers/activities/php-calc-task/src/Types/IMessageHandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Bitrix24\RabbitMQ\Types;
-
-interface IMessageHandler {
-    public function handle($msg, callable $ack, callable $nack): void;
-}
